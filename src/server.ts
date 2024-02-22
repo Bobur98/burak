90dafd08e167e84b0ef2d08133364bc05180a104
Bobur98 <bmuhibullaev@mail.ru>
2024-02-22 20:40:12 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
 console.log('EXUCUTED!');
 
+// Architectural pattern(prjecting suyagi): backend: MVC, DI, frontEnd: MVP
+
+// MVC => Model View Controler
+
+// DI => dependancy injection
+
+// MVP => Model View Presenter
+
+// Design pattern(projectning malum bir qismini arxitukturasi): Middleware, decorator
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
