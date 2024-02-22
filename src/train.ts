e69616b79a452df3fd61b7f95de6e163c6bf25a5
Bobur98 <bmuhibullaev@mail.ru>
2024-02-22 21:07:52 +0900

    task: G-TASk is done

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,21 @@
 console.log("train is EXECUTED!");
+
+// G-TASK
+
+function getHighestIndex(nums: number[]) {
+    let highestNums = 0;
+    let maxIndex = 0;
+    for(let i = 0; i <= nums.length; i++){
+        if (nums[i] > highestNums){
+            highestNums = nums[i];
+            maxIndex = i
+
+        }
+    }
+   return maxIndex
+}
+
+
+let test = [1,5,3,5]
+
+console.log(getHighestIndex(test));
