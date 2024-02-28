5665054505b02eb93298763d76af99e65cad6991
Bobur98 <bmuhibullaev@mail.ru>
2024-02-28 12:14:08 +0900

    task: H-TASK is completed

--- src/train.ts
+++ src/train.ts
@@ -1,21 +1,38 @@
 console.log("train is EXECUTED!");
 
-// G-TASK
+// H-TASK
 
-function getHighestIndex(nums: number[]) {
-    let highestNums = 0;
-    let maxIndex = 0;
-    for(let i = 0; i <= nums.length; i++){
-        if (nums[i] > highestNums){
-            highestNums = nums[i];
-            maxIndex = i
+function getPositive(arr: number[]) {
+    let positiveNums = ''
 
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] > 0) {
+           positiveNums += arr[i]   
         }
-    }
-   return maxIndex
+     }
+
+     return positiveNums
 }
 
+console.log(getPositive([1,-4,2,-1,5]));
 
-let test = [1,5,3,5]
 
-console.log(getHighestIndex(test));
+// G-TASK
+
+// function getHighestIndex(nums: number[]) {
+//     let highestNums = 0;
+//     let maxIndex = 0;
+//     for(let i = 0; i <= nums.length; i++){
+//         if (nums[i] > highestNums){
+//             highestNums = nums[i];
+//             maxIndex = i
+
+//         }
+//     }
+//    return maxIndex
+// }
+
+
+// let test = [1,5,3,5]
+
+// console.log(getHighestIndex(test));
