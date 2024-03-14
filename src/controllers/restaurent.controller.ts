14700fefa024e817dabe95903c37cab070561731
Bobur98 <bmuhibullaev@mail.ru>
2024-03-14 23:48:12 +0900

    fix: modify restaurent controller processSignup and productLogin

--- src/controllers/restaurent.controller.ts
+++ src/controllers/restaurent.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common"
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 const restaurentController: T = {};
 const memberService = new MemberService()
 
@@ -40,8 +40,13 @@ restaurentController.getSignup = (req: Request, res: Response) => {
 restaurentController.processSignup = async (req: AdminRequest, res: Response) => {
    try {
       console.log('body', req.body);
+      
+      const file = req.file;
+      if(!file)
+        throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG)
 
       const newMember: MemberInput = req.body;
+      newMember.memberImage = file?.path;
       newMember.memberType = MemberType.RESTAURANT
 
       const result = await memberService.processSignup(newMember);
@@ -50,7 +55,7 @@ restaurentController.processSignup = async (req: AdminRequest, res: Response) =>
       
       req.session.member = result
       req.session.save(function() {
-         res.send(result)
+         res.redirect('/admin/product/all')
       });
 
 
@@ -71,7 +76,7 @@ restaurentController.processLogin = async (req: AdminRequest, res: Response) => 
        //  SESSIONS AUTHENTICATION
       req.session.member = result
       req.session.save(function() {
-         res.send(result)
+         res.redirect('/admin/product/all')
       });
       
    } catch (err) {
